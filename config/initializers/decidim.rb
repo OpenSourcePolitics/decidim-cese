4cb3e67411d8ee5f89bc681aaac35db7abf3ebf7
Elie Gaboriau <elie@opensourcepolitics.eu>
2022-12-02 12:14:13 +0100

    add sms gateway service

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -72,7 +72,7 @@ Decidim.configure do |config|
   #   end
   # end
   #
-  # config.sms_gateway_service = 'Decidim::Verifications::Sms::ExampleGateway'
+  config.sms_gateway_service = "SMSGatewayService"
 
   # Etherpad configuration
   #
--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -33,4 +33,8 @@ else
   Decidim::Verifications.register_workflow(:osp_authorization_handler) do |auth|
     auth.form = "Decidim::OspAuthorizationHandler"
   end
+
+  Rails.application.initializer "sms_verification_workflow", after: "decidim.sms_verification_workflow" do
+    Decidim::Verifications.find_workflow_manifest(:sms).expires_in = 5.minutes if Decidim.sms_gateway_service
+  end
 end
