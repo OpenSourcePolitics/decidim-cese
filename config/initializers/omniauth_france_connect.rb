28b3da563d8a5b62eff163add6e465f3a12ef9da
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2023-02-01 14:35:56 +0100

    Update all server-side modifications for FranceConnect to logout when we disconnect or delete our account

--- config/initializers/omniauth_france_connect.rb
+++ config/initializers/omniauth_france_connect.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider(
+    :france_connect,
+    setup: lambda { |env|
+      request = Rack::Request.new(env)
+      organization = env["decidim.current_organization"].presence || Decidim::Organization.find_by(host: request.host)
+      provider_config = organization.enabled_omniauth_providers[:france_connect]
+      env["omniauth.strategy"].options[:client_id] = provider_config[:client_id]
+      env["omniauth.strategy"].options[:client_secret] = provider_config[:client_secret]
+      env["omniauth.strategy"].options[:site] = provider_config[:site_url]
+      env["omniauth.strategy"].options[:scope] = %w(openid email)
+    }
+  )
+end
 if defined? OmniAuth::Strategies::FranceConnectUid
-  if Rails.application.secrets.dig(:omniauth, :france_connect).present?
-    Rails.application.config.middleware.use OmniAuth::Builder do
-      provider(
-        :france_connect,
-        setup: lambda { |env|
-          request = Rack::Request.new(env)
-          organization = Decidim::Organization.find_by(host: request.host)
-          provider_config = organization.enabled_omniauth_providers[:france_connect]
-          env["omniauth.strategy"].options[:client_id] = provider_config[:client_id]
-          env["omniauth.strategy"].options[:client_secret] = provider_config[:client_secret]
-          env["omniauth.strategy"].options[:site] = provider_config[:site_url]
-        },
-        scope: [:openid, :birthdate]
-      )
-    end
-  end
 
   if Rails.application.secrets.dig(:omniauth, :france_connect_profile).present?
     Rails.application.config.middleware.use OmniAuth::Builder do
