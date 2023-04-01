4bf803bd169dd548146ea1165b8e74f7dbd64275
moustachu <moustachu@opensourcepolitics.eu>
2023-04-01 20:43:58 +0200

    add script from local to scaleway object storage

---
+++ lib/scripts/migrate_active_storage_from_local_to_scaleway.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# inspired from https://www.stefanwienert.de/blog/2018/11/05/active-storage-migrate-between-providers-from-local-to-amazon/
+
+module AsDownloadPatch
+  def open(tempdir: nil, &block)
+    ActiveStorage::Downloader.new(self, tempdir: tempdir).download_blob_to_tempfile(&block)
+  end
+end
+
+Rails.application.config.to_prepare do
+  ActiveStorage::Blob.send(:include, AsDownloadPatch)
+end
+
+def migrate(from, to)
+  configs = Rails.configuration.active_storage.service_configurations
+  from_service = ActiveStorage::Service.configure from, configs
+  to_service   = ActiveStorage::Service.configure to, configs
+
+  ActiveStorage::Blob.service = from_service
+
+  puts "#{ActiveStorage::Blob.count} Blobs to go..."
+  ActiveStorage::Blob.find_each do |blob|
+    print '.'
+    blob.open do |tf|
+      checksum = blob.checksum
+      to_service.upload(blob.key, tf, checksum: checksum)
+    end
+  end
+end
+
+migrate(:local, :scaleway)
