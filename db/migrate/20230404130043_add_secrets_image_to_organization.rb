f94e4ebc2d9b84f101972dbd7d0c39ed90779b43
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-04-06 17:19:10 +0200

    add revert possibility for migration

--- db/migrate/20230404130043_add_secrets_image_to_organization.rb
+++ db/migrate/20230404130043_add_secrets_image_to_organization.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class AddSecretsImageToOrganization < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :decidim_organizations, :official_cachet, :string
     add_column :decidim_organizations, :official_signature, :string
+  end
+
+  def down
+    remove_column :decidim_organizations, :official_cachet
+    remove_column :decidim_organizations, :official_signature
   end
 end
