863c41dd1b3904698b3144cbd70260f623225d9c
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-06-01 11:29:28 +0200

    update dockerignore

---
+++ lib/extends/commands/decidim/verifications/authorize_user_extends.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module AuthorizeUserExtends
+  extend ActiveSupport::Concern
+
+  included do
+    # Override commands to remove the verification conflict
+    def call
+      if handler.invalid?
+        return broadcast(:invalid)
+      end
+
+      Decidim::Authorization.create_or_update_from(handler)
+
+      broadcast(:ok)
+    end
+  end
+end
+
+Decidim::Verifications::AuthorizeUser.class_eval do
+  include(AuthorizeUserExtends)
+end
+
--- spec/commands/decidim/verifications/authorize_user_spec.rb
+++ spec/commands/decidim/verifications/authorize_user_spec.rb
@@ -82,34 +82,8 @@ module Decidim::Verifications
                  name: handler.handler_name)
         end
 
-        it "saves conflicts" do
-          expect { subject.call }.to change(Decidim::Verifications::Conflict, :count).by(1)
-        end
-
-        it "increases conflicts times" do
-          subject.call
-
-          conflict = Decidim::Verifications::Conflict.last
-
-          expect(conflict.times).to eq(1)
-
-          subject.call
-
-          expect(conflict.reload.times).to eq(2)
-        end
-
-        it "sends notification to admins" do
-          allow(Decidim::EventsManager).to receive(:publish).and_call_original
-          subject.call
-
-          conflict = Decidim::Verifications::Conflict.last
-
-          expect(Decidim::EventsManager).to have_received(:publish).with(
-            event: "decidim.events.verifications.managed_user_error_event",
-            event_class: Decidim::Verifications::ManagedUserErrorEvent,
-            resource: conflict,
-            affected_users: Decidim::User.where(admin: true, organization: organization)
-          )
+        it "doesn't saves conflicts" do
+          expect { subject.call }.not_to change(Decidim::Verifications::Conflict, :count)
         end
       end
     end
