a995ae06bdfef0fe141348057c9d1f92e21fc39a
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-06-19 18:32:28 +0200

    Refactor migrations tools (#329)
    # Conflicts:
    #	lib/scripts/migrate_active_storage_from_local_to_scaleway.rb
    #	lib/tasks/k8s.rake

--- lib/scripts/migrate_active_storage_from_local_to_scaleway.rb
+++ lib/scripts/migrate_active_storage_from_local_to_scaleway.rb
@@ -1,34 +1,12 @@
 # frozen_string_literal: true
 
-# inspired from https://www.stefanwienert.de/blog/2018/11/05/active-storage-migrate-between-providers-from-local-to-amazon/
+require "active_storage/migrator"
 
-module AsDownloadPatch
-  def open(tempdir: nil, &block)
-    ActiveStorage::Downloader.new(self, tempdir: tempdir).download_blob_to_tempfile(&block)
-  end
-end
-
-Rails.application.config.to_prepare do
-  ActiveStorage::Blob.include AsDownloadPatch
-end
-
-def migrate(from, to)
-  configs = Rails.configuration.active_storage.service_configurations
-  from_service = ActiveStorage::Service.configure from, configs
-  to_service = ActiveStorage::Service.configure to, configs
-
-  ActiveStorage::Blob.service = from_service
-
-  puts "#{ActiveStorage::Blob.count} Blobs to go..."
-  ActiveStorage::Blob.find_each do |blob|
-    print "."
-    blob.open do |tf|
-      checksum = blob.checksum
-      to_service.upload(blob.key, tf, checksum: checksum)
+namespace :scaleway do
+  namespace :storage do
+    desc "Migrate Active Storage from local to scaleway"
+    task migrate_from_local: :environment do
+      ActiveStorage::Migrator.migrate!(:local, :scaleway)
     end
-  rescue ActiveStorage::FileNotFoundError
-    next
   end
 end
-
-migrate(:local, :scaleway)
