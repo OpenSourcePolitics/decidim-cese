5826f7aa2e357a17b4910240a91737b67106c1c1
AyakorK <guillaume.moret@yahoo.com>
2023-11-30 14:31:45 +0100

    feat: Disable Email from Extended Authorization Handler form

---
+++ app/services/extended_socio_demographic_authorization_handler.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# Allows to create a form for simple Socio Demographic authorization
+class ExtendedSocioDemographicAuthorizationHandler < Decidim::AuthorizationHandler
+  attribute :last_name, String
+  attribute :usual_last_name, String
+  attribute :first_name, String
+  attribute :usual_first_name, String
+  attribute :birth_date, Date
+  attribute :address, String
+  attribute :postal_code, String
+  attribute :city, String
+  attribute :certification, Boolean
+  attribute :news_cese, Boolean
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :birth_date, presence: true
+  validates :address, presence: true
+  validates :postal_code, numericality: { only_integer: true }, presence: true, length: { is: 5 }
+  validates :city, presence: true
+  validates :certification, acceptance: true, presence: true
+
+  validate :over_16?
+
+  def metadata
+    super.merge(
+      last_name: last_name,
+      usual_last_name: usual_last_name,
+      first_name: first_name,
+      usual_first_name: usual_first_name,
+      address: address,
+      postal_code: postal_code,
+      city: city,
+      birth_date: birth_date,
+      certification: certification,
+      news_cese: news_cese
+    )
+  end
+
+  private
+
+  def over_16?
+    return if birth_date.blank?
+    return true if birth_date < 16.years.ago.to_date
+
+    errors.add :birth_date, I18n.t("extended_socio_demographic_authorization.errors.messages.over_16")
+  end
+end
---
+++ spec/services/extended_socio_demographic_authorization_handler/extended_socio_demographic_authorization_handler_spec.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe ExtendedSocioDemographicAuthorizationHandler do
+  subject do
+    described_class.new(
+      last_name: last_name,
+      usual_last_name: usual_last_name,
+      first_name: first_name,
+      usual_first_name: usual_first_name,
+      address: address,
+      postal_code: postal_code,
+      city: city,
+      birth_date: birth_date,
+      certification: certification,
+      news_cese: news_cese
+    )
+  end
+
+  let(:user) { create(:user) }
+
+  let(:organization) { user.organization }
+
+  let(:last_name) { "Doe" }
+  let(:usual_last_name) { "Smith" }
+  let(:first_name) { "John" }
+  let(:usual_first_name) { "Jack" }
+  let(:address) { "21 Jump Street" }
+  let(:postal_code) { "75018" }
+  let(:city) { "Nowhere" }
+  let(:birth_date) { 20.years.ago.to_date }
+  let(:certification) { true }
+  let(:news_cese) { true }
+
+  context "when the information is valid" do
+    it "is valid" do
+      expect(subject).to be_valid
+    end
+  end
+
+  context "when name is nil" do
+    let(:last_name) { nil }
+
+    it "is invalid" do
+      expect(subject).to be_invalid
+    end
+  end
+
+  context "when first name is nil" do
+    let(:first_name) { nil }
+
+    it "is invalid" do
+      expect(subject).to be_invalid
+    end
+  end
+
+  context "with postal code" do
+    context "and nil" do
+      let(:postal_code) { nil }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+
+    context "and not a number" do
+      let(:postal_code) { "Some string" }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+
+    context "and length inferior than 5 chars" do
+      let(:postal_code) { "1234" }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+
+    context "and length greater than 5 chars" do
+      let(:postal_code) { "123456" }
+
+      it "is invalid" do
+        expect(subject).to be_invalid
+      end
+    end
+  end
+
+  context "when city is nil" do
+    let(:city) { nil }
+
+    it "is invalid" do
+      expect(subject).to be_invalid
+    end
+  end
+
+  context "when certification is unchecked" do
+    let(:certification) { "0" }
+
+    it "is invalid" do
+      expect(subject).to be_invalid
+    end
+  end
+
+  context "when birthdate is nil" do
+    let(:birth_date) { nil }
+
+    it "is invalid" do
+      expect(subject).to be_invalid
+    end
+  end
+
+  context "when the person is too young" do
+    let(:birth_date) { 14.years.ago.to_date }
+
+    it "is invalid" do
+      expect(subject).to be_invalid
+    end
+  end
+end
