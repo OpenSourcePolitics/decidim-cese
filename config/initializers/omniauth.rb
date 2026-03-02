ac7317865a813b360fd8acf5511ac5859eef60fb
moustachu <moustachu@opensourcepolitics.eu>
2026-03-02 15:14:09 +0100

    fix: disabled profile edit on omniauth sign in

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+ActiveSupport::Notifications.subscribe "decidim.user.omniauth_registration" do |_name, data|
+  Rails.logger.debug "decidim.user.omniauth_registration event in config/initializers/omniauth.rb"
+
+  update_user_profile(data)
+end
+
+def update_user_profile(data)
+  user = Decidim::User.find(data[:user_id])
+
+  user.email = data[:email] if data[:email].present?
+  user.skip_reconfirmation! if data[:email].present? && user.email_changed?
+  user.name = data[:name] if data[:name].present?
+
+  user.save!(validate: false, touch: false)
+end
